429e8a711195296183cdd0305b740685ecbfb3a5
Enzo Braga <ebdalmoro@gmail.com>
2023-06-21 16:44:01 -0300

    Contador funcionando

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const trash = document.querySelector('.trash');
 const tree  = document.querySelector('.tree');
 var sec = 0;
+var ticks = 1000;
+
 
 const jump = () => {
     trash.classList.add('jump');
@@ -15,32 +17,34 @@ const loop = setInterval(() => {
 
     const treePosition = tree.offsetLeft;
     const trashPosition = window.getComputedStyle(trash).bottom.replace('px', '');
-
-function timer() {
-    sec++;
-};
-setInterval(timer, 1000);
-document.getElementById("count").innerHTML = sec;
     
     window.addEventListener('keypress', (event) => {
         if (event.key === ' ') {
             event.preventDefault();
         }
     })
-    
+    document.getElementById("count").innerHTML = sec;
     if (treePosition <= 160 && treePosition > 0 && trashPosition < 256){
 
         tree.style.animation = "none";
-        tree.style.left = `${treePosition}px`
+        tree.style.left = `${treePosition}px`;
 
         trash.style.animation = "none";
-        trash.style.left = `${trashPosition}px`
+        trash.style.left = `${trashPosition}px`;
     
-        trash.src = './sprite/Trash-die.png'
+        trash.src = './sprite/Trash-die.png';
+
+        clearInterval(cont);
     }
 
-}, 10)
+}, 10);
+
+function timer() {
+    sec = sec + 50;
+};
+var cont = setInterval(timer, ticks);
 
 document.addEventListener('keypress', jump);
+
 
 
