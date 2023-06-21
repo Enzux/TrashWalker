ed0b6ea0e2bbc7c011288337a4aa90cda9dbbc48
Enzo Braga <ebdalmoro@gmail.com>
2023-06-21 08:13:57 -0300

    Base

---
+++ script.js
@@ -0,0 +1,44 @@
+const trash = document.querySelector('.trash');
+const tree  = document.querySelector('.tree');
+var pulos = 0;
+
+const jump = () => {
+    trash.classList.add('jump');
+
+    setTimeout(() => {
+        trash.classList.remove('jump');
+
+    }, 700);
+}   
+
+const loop = setInterval(() => {
+
+    const treePosition = tree.offsetLeft;
+    const trashPosition = window.getComputedStyle(trash).bottom.replace('px', '');
+   
+    window.addEventListener('keypress', (event) => {
+        if (event.key === 'w') {
+            event.preventDefault();
+            pulos++;
+            console.log('pulos');
+        }
+    })
+    
+    if (treePosition <= 160 && treePosition > 0 && trashPosition < 256){
+
+        tree.style.animation = "none";
+        tree.style.left = `${treePosition}px`
+
+        trash.style.animation = "none";
+        trash.style.left = `${trashPosition}px`
+    
+        trash.src = './sprite/Trash-die.png'
+        
+        
+    }
+
+}, 10)
+
+document.addEventListener('keypress', jump);
+
+
